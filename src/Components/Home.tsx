68f51bd9872c87f7d8192e9f8a5eff51e718c26a
wyt <xuziling111@gmail.com>
2019-12-27 16:58:30 +0800

    Home Page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-
+import '../App.css';
+import Grid from 'antd/lib/card/Grid';
+import { Row, Col } from 'antd';
 export class Home extends Component {
-  
   render() {
     return (
-      <h1>This is home</h1>
+      <div className="allBackground">
+        <div className="background">
+          <div className="introduction">
+            <div >
+            介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍
+            </div>
+          </div>
+        </div>
+        <div style={{height:"300px"}}>
+          <Row>
+            <Col span={2}></Col>
+            <Col span={9}>{box()}</Col>
+            <Col span={2}></Col>
+            <Col span={9}>{box()}</Col>
+            <Col span={2}></Col>
+          </Row>
+        </div>
+      </div>
     );
   }
+}
+
+const box = ()=>{
+  return(
+    <div className="box">
+    </div>
+  );
 }
