94cb0fd4997ce54a94244d9ff80679bee51a5191
Ben Feng <fybmain@gmail.com>
2019-12-28 09:58:25 +0800

    添加报销审批页面

--- src/Components/ReimbursementExaminePage.tsx
+++ src/Components/ReimbursementExaminePage.tsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 
 export class ReimbursementExaminePage extends Component {
   
   render() {
+    const columns = [
+      {
+        title: '申请人',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '申请时间',
+        dataIndex: 'time',
+        key: 'time',
+      },
+      {
+        title: '申请状态',
+        dataIndex: 'status',
+        key: 'status',
+      },
+    ];
+    const data = [
+      {
+        name: '张可',
+        time: '2019-12-27 11:00:00',
+        status: '待部门经理审核',
+      },
+      {
+        name: '赵可',
+        time: '2019-12-28 12:00:00',
+        status: '待总经理审核',
+      },
+    ];
     return (
-      <h1>This is reimbursement examine page</h1>
+      <Table columns={columns} dataSource={data}/>
     );
   }
 }
