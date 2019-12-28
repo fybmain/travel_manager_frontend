45edf2f99c5bde308e0826510bebce04342143cf
Ben Feng <fybmain@gmail.com>
2019-12-28 09:54:14 +0800

    添加出差审批页面

--- src/Components/TravelExaminePage.tsx
+++ src/Components/TravelExaminePage.tsx
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 
 export class TravelExaminePage extends Component {
   
   render() {
+    const columns = [
+      {
+        title: '申请人',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '申请时间',
+        dataIndex: 'time',
+        key: 'time',
+      },
+      {
+        title: '申请状态',
+        dataIndex: 'status',
+        key: 'status',
+      },
+    ];
+    const data = [
+      {
+        name: '张可',
+        time: '2019-12-24 10:00:00',
+        status: '待部门经理审核',
+      },
+      {
+        name: '赵可',
+        time: '2019-12-25 11:00:00',
+        status: '待总经理审核',
+      },
+      {
+        name: '钱可',
+        time: '2019-12-26 12:00:00',
+        status: '待部门经理审核',
+      },
+      {
+        name: '孙可',
+        time: '2019-12-27 13:00:00',
+        status: '审核通过',
+      },
+      {
+        name: '李可',
+        time: '2019-12-28 14:00:00',
+        status: '审核未通过',
+      },
+    ];
     return (
-      <h1>This is travel examine page</h1>
+      <Table columns={columns} dataSource={data}/>
     );
   }
 }
