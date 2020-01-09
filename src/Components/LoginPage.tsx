c822af944038b1b458393dc775b7b3d8bcbdf0e5
Ben Feng <fybmain@gmail.com>
2020-01-09 11:00:36 +0800

    登录界面添加表单校验

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Input, Icon, Row, Button, message } from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
 import { observer, inject } from 'mobx-react';
 import { observable } from 'mobx';
 
@@ -9,15 +10,11 @@ import { MainStore } from '../Stores/MainStore';
 import UserInfoStore from '../Stores/UserInfoStore';
 import { ForgetPasswordDialog } from './ForgetPasswordDialog';
 
-interface LoginPageProps {
+interface LoginPageProps extends FormComponentProps {
   mainStore: MainStore;
 }
 @inject("mainStore") @observer
 export class LoginPage extends React.Component<LoginPageProps> {
-  @observable registerDialogVisible = false;
-  @observable forgetPasswordDialogVisible = false;
-  workId = "";
-  password = "";
 
   /*
   constructor(props: LoginPageProps) {
@@ -25,75 +22,117 @@ export class LoginPage extends React.Component<LoginPageProps> {
   }
   */
 
-  handleLogin = async (e: React.MouseEvent) => {
-    const result = await UserInfoStore.login({ workId: this.workId, password: this.password });
-    if (result !== "ok") {
-      message.error(result);
-    }
-  }
-
   render() {
     return (
       <div className="login-background">
         <div className="login-content">
           <h1 style={{ textAlign: "center" }}>登录</h1>
-          <Form
-            layout="horizontal">
-
-            <Form.Item>
-              <Input
-                placeholder="工号"
-                prefix={<Icon type="workId" />}
-                size="large"
-                onChange={(e) => { this.workId = e.target.value; }}>
-              </Input>
-            </Form.Item>
-
-            <Form.Item>
-              <Input.Password
-                placeholder="密码"
-                prefix={<Icon type="password" />}
-                size="large"
-                onChange={(e) => { this.password = e.target.value; }}>
-              </Input.Password>
-            </Form.Item>
-
-
-            <Row style={{ marginTop: 0 }}>
-              <button
-                type="button"
-                className="button-like-link"
-                style={{ float: "right" }}
-                onClick={() => {this.registerDialogVisible = true;}}>
-                注册
-              </button>
-              <button
-                type="button"
-                className="button-like-link"
-                style={{ float: "left" }}
-                onClick={() => {this.forgetPasswordDialogVisible = true;}}>
-                忘记密码
-              </button>
-            </Row>
-
-            <Form.Item>
-              <Button
-                onClick={this.handleLogin}
-                type="primary"
-                htmlType="submit"
-                style={{ width: "100%" }}>
-                立即登录
-            </Button>
-            </Form.Item>
-          </Form>
-          <RegisterDialog
-            visible={this.registerDialogVisible}
-            onClose={() => {this.registerDialogVisible = false;}} />
-          <ForgetPasswordDialog
-            visible={this.forgetPasswordDialogVisible}
-            onClose={() => {this.forgetPasswordDialogVisible = false;}} />
+          <LoginForm/>
         </div>
       </div>
     );
   }
 }
+
+@observer
+class LoginFormProto extends React.Component<FormComponentProps> {
+  @observable registerDialogVisible = false;
+  @observable forgetPasswordDialogVisible = false;
+
+  handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll(async (err, values) => {
+      if (!err) {
+        const result = await UserInfoStore.login({ workId: values.workId, password: values.password });
+        if (result !== "ok") {
+          message.error(result);
+        }
+      }
+    });
+
+  }
+
+  render() {
+    return (
+      <Form
+        layout="horizontal"
+        onSubmit={this.handleSubmit}>
+
+        <Form.Item>
+          {
+            this.props.form.getFieldDecorator('workId', {
+              rules: [
+                {
+                  required: true,
+                  message: '工号不能为空',
+                },
+              ]
+            })(
+              <Input
+                placeholder="工号"
+                prefix={<Icon type="workId" />}
+                size="large">
+              </Input>
+            )
+          }
+        </Form.Item>
+
+        <Form.Item>
+          {
+            this.props.form.getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: '密码不能为空',
+                },
+              ]
+            })(
+              <Input.Password
+                placeholder="密码"
+                prefix={<Icon type="password" />}
+                size="large">
+              </Input.Password>
+            )
+          }
+
+        </Form.Item>
+
+
+        <Row style={{ marginTop: 0 }}>
+          <button
+            onClick={() => {this.registerDialogVisible = true;}}
+            type="button"
+            className="button-like-link"
+            style={{ float: "right" }}>
+            注册
+          </button>
+          <button
+            onClick={() => {this.forgetPasswordDialogVisible = true;}}
+            type="button"
+            className="button-like-link"
+            style={{ float: "left" }}>
+            忘记密码
+          </button>
+        </Row>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ width: "100%" }}>
+            立即登录
+        </Button>
+        </Form.Item>
+
+        <RegisterDialog
+          visible={this.registerDialogVisible}
+          onClose={() => {this.registerDialogVisible = false;}} />
+        <ForgetPasswordDialog
+          visible={this.forgetPasswordDialogVisible}
+          onClose={() => {this.forgetPasswordDialogVisible = false;}} />
+      </Form>
+    );
+  }
+}
+
+const LoginForm = Form.create({ name: 'login' })(LoginFormProto)
