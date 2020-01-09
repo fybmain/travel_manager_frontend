3b9bfee9fef3484e958cac25841cfdecf358d32b
Ben Feng <fybmain@gmail.com>
2020-01-09 12:14:10 +0800

    添加获取消息API

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -2,6 +2,7 @@ export * from './Common';
 
 export * from './Apply';
 export * from './DepartmentInfo';
+export * from './Message';
 export * from './Reimbursement';
 export * from './TravelApply';
 export * from './User';
