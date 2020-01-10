47f62b9c07ad6885a8555c1d9320f0a4b3edbe4e
wyt <xuziling111@gmail.com>
2020-01-10 16:28:49 +0800

    改掉柱状图的选择器不在一行的bug

--- src/Components/Reports/CostChangeChart.tsx
+++ src/Components/Reports/CostChangeChart.tsx
@@ -68,28 +68,29 @@ export class CostChangeChart extends React.Component<CostChangeChartProps> {
 
   render() {
     return (
-      <Card title="过去一年预算与实际费用对比图" className="card-high">
-        <div style={{ display: "inline", width: 100, height: 30, marginLeft: 30 }}>
-          <label>&nbsp;&nbsp;&nbsp;费用类型：</label>
-          <Select
-            style={{ width: 120 }}
-            defaultValue={'all'}
-            onChange={this.handleSelectChange}
-            key="select">
-            <Option value="all">所有</Option>
-            <Option value="food">饮食</Option>
-            <Option value="hotel">酒店</Option>
-            <Option value="vehicle">车旅</Option>
-            <Option value="other">其他</Option>
-          </Select>
+      <Card title="过去一年预算与实际费用对比图" className="card">
+        <div>
+          <div style={{ display: "inline", width: 100, height: 30, marginLeft: 30 }}>
+            <label>&nbsp;&nbsp;&nbsp;费用类型：</label>
+            <Select
+              style={{ width: 120 }}
+              defaultValue={'all'}
+              onChange={this.handleSelectChange}
+              key="select">
+              <Option value="all">所有</Option>
+              <Option value="food">饮食</Option>
+              <Option value="hotel">酒店</Option>
+              <Option value="vehicle">车旅</Option>
+              <Option value="other">其他</Option>
+            </Select>
+          </div>
+          <div style={{ display: "inline", width: 100, height: 30 }}>
+            {
+              this.props.mainStore.breadcrumb[1] === "公司报表" ?
+                <DepartmentSelector changeSelect={this.changeSelect} />
+                : null
+            }</div>
         </div>
-        <br/><br/>
-        <div style={{ display: "inline", width: 100, height: 30 }}>
-          {
-            this.props.mainStore.breadcrumb[1] === "公司报表" ?
-              <DepartmentSelector changeSelect={this.changeSelect} />
-              : null
-          }</div>
         <br /><br />
         {
           this.loadingStatus ?
